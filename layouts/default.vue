fe25e2505c25137bdfcaac94f9745459285bd9ff
Aleja Ocampo <omaira.527@hotmail.com>
2021-07-09 22:47:12 -0500

    jugando con prismic

---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h2>Jugando con prismic</h2>
+    <nuxt/>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+ <div>
+   <div>
+    <h1 v-text="data.title[0].text"></h1>
+    <p v-text="data.text[0].text"></p>
+    
+    <img :src="data.image.url"/>
+  </div>
+ <div>
+   <h3 v-text="data.title[0].text"></h3>
+ </div>
+ </div>
+</template>
+
+<script>
+export default {
+ async asyncData( {$prismic}){
+     const {data} = await $prismic.api.getSingle('simplepage');
+    console.log("Hola soy la data", data)
+     return  {data }
+  }
+}
+</script>
